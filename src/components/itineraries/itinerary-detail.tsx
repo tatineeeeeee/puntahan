37175da19242258483aa217ad7fa02ed303ab664
Justine Cesar L. Ocampo <justinecesarocampo@gmail.com>
2026-04-02 06:51:19 +0800

    Remove unused Textarea import from itinerary-detail

--- src/components/itineraries/itinerary-detail.tsx
+++ src/components/itineraries/itinerary-detail.tsx
@@ -6,7 +6,6 @@ import { api } from "../../../convex/_generated/api";
 import { Id } from "../../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
